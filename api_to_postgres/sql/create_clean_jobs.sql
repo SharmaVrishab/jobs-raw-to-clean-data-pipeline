142455de375166aa7b892b792dd0f1e43b603bc1
securitylamb <vrishabsharma1111@gmail.com>
2026-01-19 14:04:07 +0530

    completed phase 2 and added sql script for clean jobs

---
+++ api_to_postgres/sql/create_clean_jobs.sql
@@ -0,0 +1,34 @@
+-- Phase 2: Create clean, queryable jobs table
+
+
+create table if not exists clean_jobs (
+    job_id text primary key ,
+    company_name text,
+    title text,
+    location text,
+    remote boolean,
+    url text ,
+    created_at timestamp
+);
+
+
+insert into clean_jobs
+ (
+    job_id,
+    company_name,
+    title,
+    location,
+    remote,url,
+    created_at
+    )
+SELECT DISTINCT ON (job->>'slug')
+    job->>'slug'                    AS job_id,
+    job->>'company_name'            AS company_name,
+    job->>'title'                   AS title,
+    job->>'location'                AS location,
+    (job->>'remote')::BOOLEAN       AS remote,
+    job->>'url'                     AS url,
+    TO_TIMESTAMP((job->>'created_at')::BIGINT)
+FROM raw_jobs,
+     LATERAL jsonb_array_elements(raw_payload->'data') AS job;
+
